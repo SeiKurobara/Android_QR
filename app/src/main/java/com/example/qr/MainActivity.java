c3dbd9833baa95faa937c3602373f4d10cb16237
SeiKurobara <seijinkurobara@gmail.com>
2024-02-19 18:33:46 +0800

    main

---
+++ app/src/main/java/com/example/qr/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.qr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "Kangkong", Toast.LENGTH_SHORT).show();
+
+
+    }
+}
